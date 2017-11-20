04bb5bc530f68f127f018c909e170f68f2f81ba5
songtao <songtao_st08@163.com>
2017-11-20 16:17:28 +0800

    20

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 var webpack = require('webpack'),
+    path=require('path'),
+    ROOT_PATH=path.resolve(__dirname),
+    APP_PATH=path.resolve(ROOT_PATH,'src'),
     ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var plugins = [
-    new ExtractTextPlugin("styles.css")
-];
+var plugins = [];
 // plugins.push(new webpack.optimize.CommonsChunkPlugin({
 //     name:"commonjs",
 //     filename:"common.[chunkhash].js"
 // }));
 plugins.push(new UglifyJSPlugin());
+plugins.push( new ExtractTextPlugin("styles.css"));
 var webpackConfig = {
     output: {
         path: __dirname+'/dist/static',
@@ -18,8 +20,11 @@ var webpackConfig = {
     module: {
         loaders: [
             {test: /\.html$/, loader: 'html-loader'},
-            {test: /\.less$/, loader:  ExtractTextPlugin.extract("style-loader", "css-loader", "less-loader")},
-            {test: /\.css/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
+            {
+                test: /\.less$/,
+                loader:  ExtractTextPlugin.extract("style-loader", "css-loader", "less-loader"),
+                include: path.resolve(APP_PATH)
+            },
             {test: /\.ejs/, loader: 'ejs-loader?variable=data'}
         ]
     },
